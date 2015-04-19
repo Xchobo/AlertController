7f5ee3f9478406c43ffe8919e3ffade143a9a659
code <code@HG202-88deT1.local>
2015-04-19 13:42:07 +0800

    Initial Commit

---
+++ AlertController/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AlertController
+//
+//  Created by code on 2015/4/19.
+//  Copyright (c) 2015年 code. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
